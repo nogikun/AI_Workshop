f572114679c77c0a46a351a7d57fe376d98d3eee
nogi <94681885+nogikun@users.noreply.github.com>
2023-12-06 08:43:53 +0000

    Edit tuning.py (OpenAIの)

--- System/tuning_req.py
+++ System/tuning_req.py
@@ -1,31 +1,40 @@
 try:
-    import openai
+    from openai import OpenAI
 except:
     print("ライブラリがインポートできませんでした。プログラムを終了します。")
     exit()
 
-# APIキーを設定
-openai.api_key = input('OpenAIのAPIキーを入力してください:')
+# Setting Client
+client = OpenAI(
+    # APIキーを設定
+    api_key=input('OpenAIのAPIキーを入力してください:')
+)
+
+
+#client.api_key = input('OpenAIのAPIキーを入力してください:')
 file_path = input('ファイルパスを教えてください（ファイル名を除く）：')
 
-# チューニングデータをアップロード
-file_response = openai.File.create(
-  file=open(f"{file_path}/TuningData.jsonl", "rb"),
-  purpose='fine-tune'
-)
-file_id = file_response['id']
-
-# 学習実行
+# ハイパーパラメータを設定
 params = {
     "n_epochs":3 # n_epochs = 3 の場合、学習時間は30分程度
     }
 
-fine_tuning_response = openai.FineTuningJob.create(
+# チューニングデータをアップロード
+file_response = client.files.create(
+  file=open(f"{file_path}/TuningData.jsonl", "rb"),
+  purpose='fine-tune'
+)
+# アップロードしたファイルのIDを保存
+file_id = file_response.id
+
+# 学習実行
+fine_tuning_response = client.fine_tuning.jobs.create(
   training_file=file_id,
   model="gpt-3.5-turbo",
   hyperparameters=params
 )
-job_id = fine_tuning_response['id']
+# ジョブIDを保存
+job_id = fine_tuning_response.id
 
 # ジョブIDを出力
 print(f"Job ID: {job_id}")
